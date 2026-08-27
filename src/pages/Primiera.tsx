bfa4f1dae3374983c21f93f0b276f19d7bcca4f5
Kim Robinson <kimmykokonut@gmail.com>
2026-08-27 14:01:29 -0700

    calculate scores for multiple players and display winner

--- src/pages/Primiera.tsx
+++ src/pages/Primiera.tsx
@@ -5,7 +5,7 @@ import spade from "../assets/spade.png";
 import club from "../assets/club.png";
 
 function Primiera() {
-  const [score, setScore] = useState<number | null>(null);
+  const [currentScore, setCurrentScore] = useState<number | null>(null);
   const [numPlayers, setNumPlayers] = useState<number | null>(null);
   const [currentPlayer, setCurrentPlayer] = useState(1);
   const [playerScores, setPlayerScores] = useState<number[]>([]);
@@ -22,9 +22,9 @@ function Primiera() {
     face: 10,
   };
 
-  const handleCalculate = (e: React.SubmitEvent) => {
+  const handleCalculate = (event: SubmitEvent) => {
     e.preventDefault();
-    const formData = new FormData(e.target);
+    const formData = new FormData(event.target as HTMLFormElement);
     const data = Object.fromEntries(formData);
     console.log(data);
 
@@ -34,12 +34,18 @@ function Primiera() {
     const clubsValue = primieraValues[data.clubs as string];
 
     const total = coinsValue + cupsValue + swordsValue + clubsValue;
-    setScore(total);
+    setCurrentScore(total);
+    setPlayerScores([...playerScores, total]);
+    console.log("player scores", playerScores);
+    // trigger final screen
+    if (currentPlayer === numPlayers) {
+      setShowResults(true);
+    }
   };
 
+  // prompt user choose # players. TODO extract to component.
   if (!numPlayers) {
     return (
-      // extract to Component. modal?
       <>
         <p>How many players?</p>
         <button onClick={() => setNumPlayers(2)}>2</button>
@@ -49,86 +55,124 @@ function Primiera() {
     );
   }
 
-  return (
-    <>
-      <h1>Primiera Calculator</h1>
-      <div style={{ display: "flex", gap: "20px" }}>
-        <img
-          src={coin}
-          alt="coin suit"
-          style={{ width: "25%", height: "25%" }}
-        />
-        <img
-          src={cup}
-          alt="cups suit"
-          style={{ width: "25%", height: "25%" }}
-        />
-        <img
-          src={spade}
-          alt="spades suit"
-          style={{ width: "25%", height: "25%" }}
-        />
-        <img
-          src={club}
-          alt="club suit"
-          style={{ width: "25%", height: "25%" }}
-        />
-      </div>
-      <form onSubmit={handleCalculate}>
-        <label htmlFor="coins">Coins (Denari)</label>
-        <select name="coins" id="coins">
-          <option value="seven">7</option>
-          <option value="six">6</option>
-          <option value="ace">A</option>
-          <option value="five">5</option>
-          <option value="four">4</option>
-          <option value="three">3</option>
-          <option value="two">2</option>
-          <option value="face">King, Queen, Jack</option>
-        </select>
+  // final page with results. animation?
+  if (showResults) {
+    return (
+      <>
+        <h1>Final Results</h1>
+        <p>Winner: Player X</p>
+        <p>{Math.max(...playerScores)}</p>
+        <ul>
+          {playerScores.map((score, player) => (
+            <li key={player}>
+              Player {player + 1} : Score {score}
+            </li>
+          ))}
+        </ul>
+      </>
+    );
+  }
 
-        <label htmlFor="cups">Cups (Coppe)</label>
-        <select name="cups" id="cups">
-          <option value="seven">7</option>
-          <option value="six">6</option>
-          <option value="ace">A</option>
-          <option value="five">5</option>
-          <option value="four">4</option>
-          <option value="three">3</option>
-          <option value="two">2</option>
-          <option value="face">King, Queen, Jack</option>
-        </select>
+  // calculate 1 score
+  if (numPlayers && !showResults) {
+    return (
+      <>
+        <h1>Primiera Calculator</h1>
+        <h3>Player {currentPlayer}</h3>
+        <div style={{ display: "flex", gap: "20px" }}>
+          <img
+            src={coin}
+            alt="coin suit"
+            style={{ width: "25%", height: "25%" }}
+          />
+          <img
+            src={cup}
+            alt="cups suit"
+            style={{ width: "25%", height: "25%" }}
+          />
+          <img
+            src={spade}
+            alt="spades suit"
+            style={{ width: "25%", height: "25%" }}
+          />
+          <img
+            src={club}
+            alt="club suit"
+            style={{ width: "25%", height: "25%" }}
+          />
+        </div>
+        <form onSubmit={handleCalculate}>
+          <label htmlFor="coins">Coins (Denari)</label>
+          <select name="coins" id="coins">
+            <option value="seven">7</option>
+            <option value="six">6</option>
+            <option value="ace">A</option>
+            <option value="five">5</option>
+            <option value="four">4</option>
+            <option value="three">3</option>
+            <option value="two">2</option>
+            <option value="face">King, Queen, Jack</option>
+          </select>
 
-        <label htmlFor="swords">Swords (Spade)</label>
-        <select name="swords" id="swords">
-          <option value="seven">7</option>
-          <option value="six">6</option>
-          <option value="ace">A</option>
-          <option value="five">5</option>
-          <option value="four">4</option>
-          <option value="three">3</option>
-          <option value="two">2</option>
-          <option value="face">King, Queen, Jack</option>
-        </select>
+          <label htmlFor="cups">Cups (Coppe)</label>
+          <select name="cups" id="cups">
+            <option value="seven">7</option>
+            <option value="six">6</option>
+            <option value="ace">A</option>
+            <option value="five">5</option>
+            <option value="four">4</option>
+            <option value="three">3</option>
+            <option value="two">2</option>
+            <option value="face">King, Queen, Jack</option>
+          </select>
 
-        <label htmlFor="clubs">Clubs (Bastoni)</label>
-        <select name="clubs" id="clubs">
-          <option value="seven">7</option>
-          <option value="six">6</option>
-          <option value="ace">A</option>
-          <option value="five">5</option>
-          <option value="four">4</option>
-          <option value="three">3</option>
-          <option value="two">2</option>
-          <option value="face">King, Queen, Jack</option>
-        </select>
+          <label htmlFor="swords">Swords (Spade)</label>
+          <select name="swords" id="swords">
+            <option value="seven">7</option>
+            <option value="six">6</option>
+            <option value="ace">A</option>
+            <option value="five">5</option>
+            <option value="four">4</option>
+            <option value="three">3</option>
+            <option value="two">2</option>
+            <option value="face">King, Queen, Jack</option>
+          </select>
 
-        <input type="submit" value="Calculate" />
-      </form>
+          <label htmlFor="clubs">Clubs (Bastoni)</label>
+          <select name="clubs" id="clubs">
+            <option value="seven">7</option>
+            <option value="six">6</option>
+            <option value="ace">A</option>
+            <option value="five">5</option>
+            <option value="four">4</option>
+            <option value="three">3</option>
+            <option value="two">2</option>
+            <option value="face">King, Queen, Jack</option>
+          </select>
 
-      {score !== null && <div>Primiera Score: {score}</div>}
-    </>
-  );
+          <input type="submit" value="Calculate" />
+        </form>
+
+        {currentScore !== null && (
+          <div>
+            <p>
+              Player {currentPlayer} score: {currentScore}
+            </p>
+          </div>
+        )}
+        {currentScore && (
+          <button
+            onClick={() => {
+              setCurrentPlayer(currentPlayer + 1);
+              setCurrentScore(null);
+            }}
+          >
+            Next Player
+          </button>
+        )}
+      </>
+    );
+  }
 }
 
 export default Primiera;
