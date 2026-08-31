fbbadc5ce03af1751cfa1db01c9a5f87e7837301
Kim Robinson <kimmykokonut@gmail.com>
2026-08-31 16:28:31 -0700

    update and fix types

--- src/pages/Primiera.tsx
+++ src/pages/Primiera.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Link } from "react-router";
 import cup from "../assets/cup.png";
 import coin from "../assets/coin.png";
 import spade from "../assets/spade.png";
@@ -10,7 +11,24 @@ function Primiera() {
   const [currentPlayer, setCurrentPlayer] = useState(1);
   const [playerScores, setPlayerScores] = useState<number[]>([]);
 
-  const primieraValues: Record<string, number> = {
+  type CardValue =
+    | "seven"
+    | "six"
+    | "ace"
+    | "five"
+    | "four"
+    | "three"
+    | "two"
+    | "face";
+
+  interface PrimieraFormData {
+    coins: CardValue;
+    cups: CardValue;
+    swords: CardValue;
+    clubs: CardValue;
+  }
+
+  const primieraValues: Record<CardValue, number> = {
     seven: 21,
     six: 18,
     ace: 16,
@@ -37,11 +55,22 @@ function Primiera() {
   const handleCalculate = (event: SubmitEvent) => {
     event.preventDefault();
     const formData = new FormData(event.target as HTMLFormElement);
-    const data = Object.fromEntries(formData);
-    const coinsValue = primieraValues[data.coins as string];
-    const cupsValue = primieraValues[data.cups as string];
-    const swordsValue = primieraValues[data.swords as string];
-    const clubsValue = primieraValues[data.clubs as string];
+
+    const data: PrimieraFormData = {
+      coins: formData.get("coins") as CardValue,
+      cups: formData.get("cups") as CardValue,
+      swords: formData.get("swords") as CardValue,
+      clubs: formData.get("clubs") as CardValue,
+    };
+
+    // don't calculate if field is missing
+    if (!data.coins || !data.cups || !data.swords || !data.clubs) {
+      return;
+    }
+    const coinsValue = primieraValues[data.coins];
+    const cupsValue = primieraValues[data.cups];
+    const swordsValue = primieraValues[data.swords];
+    const clubsValue = primieraValues[data.clubs];
 
     const total = coinsValue + cupsValue + swordsValue + clubsValue;
     setCurrentScore(total);
@@ -72,7 +101,6 @@ function Primiera() {
     return (
       <>
         <p>How many players?</p>
-        {/* TODO: make helper function to pass #players, reset state,  */}
         <button onClick={() => setNumPlayers(2)}>2</button>
         <button onClick={() => setNumPlayers(3)}>3</button>
         <button onClick={() => setNumPlayers(4)}>4</button>
@@ -103,7 +131,6 @@ function Primiera() {
             </li>
           ))}
         </ul>
-
         <button onClick={() => resetSamePlayers()}>
           Score Again (same number players)
         </button>
