195a1f84143514d14cb9f0b7159f5cf2f5105be6
Kim Robinson <kimmykokonut@gmail.com>
2026-09-04 17:29:35 -0700

    switch to hashrouter to appease gh-pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
-import { createBrowserRouter } from "react-router";
+import { createHashRouter } from "react-router";
 import { RouterProvider } from "react-router/dom";
 import Layout from "./components/Layout";
 import Home from "./pages/Home";
 import Primiera from "./pages/Primiera";
 import ScoreCard from "./pages/ScoreCard";
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: "/",
     element: <Layout />,
     children: [
       { index: true, element: <Home /> },
-      { path: "/primiera", element: <Primiera /> },
-      { path: "/score", element: <ScoreCard /> },
+      { path: "primiera", element: <Primiera /> },
+      { path: "score", element: <ScoreCard /> },
     ],
   },
 ]);
